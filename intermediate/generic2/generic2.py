0e849a59dcb66f6d3b94951ffd29b3c6e71673c3
akhmadiev <akhmadiev@tax-monitor.ru>
2025-12-30 13:31:48 +0400

    feat: добалвены 3 ответа

---
+++ intermediate/generic2/generic2.py
@@ -0,0 +1,10 @@
+"""
+The function `add` accepts two arguments and returns a value, they all have the same type.
+The type can only be str or int.
+"""
+from typing import TypeVar
+
+T = TypeVar("T", bound=int)
+
+def add(a: T) -> T:
+    return a
---
+++ intermediate/generic3/generic2.py
@@ -0,0 +1,10 @@
+"""
+The function `add` accepts two arguments and returns a value, they all have the same type.
+The type can only be str or int.
+"""
+from typing import TypeVar
+
+T = TypeVar("T", int, str)
+
+def add(a: T, b: T) -> T:
+    return a
